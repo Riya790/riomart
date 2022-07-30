fe7f9167ee4f3905b94f5b61cefdd3d2173f509c
Riya Mandal <72391917+Riya790@users.noreply.github.com>
2022-07-30 20:43:16 +0530

    Update contact-us.js

--- src/api/contact-us.js
+++ src/api/contact-us.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const ContactUs = async (email,message) => {
     const {data} = axios({
         method:'POST',
-        url:"http://localhost:8080/api/contact",
+        url:"https://xenon-task.herokuapp.com/api/contact",
         data : {
             email : email,
             message : message
